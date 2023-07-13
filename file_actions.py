ed5980b04e23c2c1c4e25813ec8e77d713ea1211
rodrigo zavala <rodrigo.zavala@troquer.com.mx>
2023-07-12 22:15:58 -0600

    [modified] main.py and file_actions.py

--- file_actions.py
+++ file_actions.py
@@ -1,7 +1,18 @@
+import os
+import json
+
+
+
 def create_file(file_name, content=None):
     """Create file with or without content"""
     mode = "w" if content else "x"
-    file = open(file_name, mode)
+    try:
+        file = open(file_name, mode)
+    except FileExistsError as error:
+        raise OSError(f'File "{file_name}" already exists') from error
+    except PermissionError as error:
+        raise OSError(f'You do not have permission to create "{file_name}"') from error
+
     if content:
        file.write(content)
     file.close()
@@ -11,6 +22,9 @@ def create_file(file_name, content=None):
 def modify__file(file_name, content, overwrite=False):
     """Modify file_name file.
     if overwrite is true, overwrite the file, if not, just update the file."""
+    if not isinstance(content, str) or content == "":
+        raise ValueError("Content agument must be specified")  #Stops the execution of the function
+
     mode = "w" if overwrite else "a"
     file = open(file_name, mode)
     file.write(" " + content)
@@ -18,9 +32,14 @@ def modify__file(file_name, content, overwrite=False):
 
 
 
-def read_file(file_name, mode='r'):
-    """Read file´s content and returns content"""
-    file = open(file_name, mode)
-    output_file = file.read()
+def read_file(file_name):
+    """Read file´s content and returns it"""
+    if not os.path.exists(file_name):
+        raise FileNotFoundError(f"File {file_name} was not found")
+
+    file = open(file_name)
+    output = file.read()
+    if output == "":
+        raise ValueError(f"Not data found in file '{file_name}'")
     file.close()
-    return output_file
+    return output
--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import file_actions as m
+import json
 
 
-m.create_file("sample.txt","some_text")
-m.modify__file("sample.txt", "Some more text added", False)
-print(m.read_file("sample.txt"))
+user1 = {"username": "rock", "name":"rodri", "l_name":"zav"}
+user2 = {"username": "rock2", "name":"rodri2", "l_name":"zav2"}
+user_list = [user1, user2]
+json_user_list = json.dumps(user_list)
+
+try:
+  m.create_file("sample.txt","")
+except OSError as error:
+  print("Could not create file -> ", error)
+
+
+try:
+  m.modify__file("sample.txt", json_user_list, False)
+except ValueError as error:
+  print("Could not update file -> " , error)
+
+
+try:
+  output = m.read_file("sample.txt")
+  print(f'Text Contained -> {output}')
+except ValueError as error:
+  print("Could not read file -> " , error)
+
+print("Program Done!!")
